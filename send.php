efb961d1dc70b10c3c583ff58d772f58edae9dbd
smurkino0 <jozef.cis08@gmail.com>
2025-08-09 15:17:18 +0200

    Update send.php

--- send.php
+++ send.php
@@ -1,26 +1,41 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $to = "kontakt@worklessai.pl"; // Twój Zoho Mail
-    $subject = "Nowa wiadomość z formularza WorkLess AI";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    $name = strip_tags($_POST["name"]);
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $message = strip_tags($_POST["message"]);
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-    $body = "Imię i nazwisko: $name\n";
-    $body .= "E-mail: $email\n\n";
-    $body .= "Wiadomość:\n$message\n";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
-    $headers = "From: no-reply@worklessai.pl\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+    $mail = new PHPMailer(true);
 
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Dziękujemy! Twoja wiadomość została wysłana.";
-    } else {
-        echo "Błąd: wiadomość nie mogła zostać wysłana.";
+    try {
+        // Konfiguracja SMTP Zoho
+        $mail->isSMTP();
+        $mail->Host = 'smtp.zoho.eu';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'kontakt@worklessai.pl';
+        $mail->Password = 'Formularz WWW';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Nadawca i odbiorca
+        $mail->setFrom('kontakt@worklessai.pl', 'WorkLess AI');
+        $mail->addAddress('kontakt@worklessai.pl');
+
+        // Treść
+        $mail->isHTML(true);
+        $mail->Subject = "Nowa wiadomość z formularza";
+        $mail->Body = "<b>Imię:</b> $name <br> <b>Email:</b> $email <br><br> <b>Wiadomość:</b> <br> $message";
+
+        $mail->send();
+        echo "Wiadomość wysłana pomyślnie!";
+    } catch (Exception $e) {
+        echo "Błąd podczas wysyłki: {$mail->ErrorInfo}";
     }
-} else {
-    echo "Błąd: nieprawidłowe żądanie.";
 }
 ?>
